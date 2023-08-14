7d1b53637bfc5b818d2644dcfbdd41a9239d2f8e
Dipesh <kvdipesh@gmail.com>
2023-08-14 09:53:17 +0530

    Update: signup framework added

--- src/Challanges/Signup/Signup.js
+++ src/Challanges/Signup/Signup.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
 function Signup() {
-  return(
-    <h1>Signup Page</h1>
+  function handleSignup(event) {
+    event.preventDefault();
+  }
+  function handleLogin(event) {
+    event.preventDefault();
+  }
+  return (
+    <div className="signup-main">
+      <main>
+        <section>
+          <h2>Login</h2>
+          <form>
+            <input type="email" placeholder='Email' name='email' />
+            <input type="password" placeholder='Password' name='password' />
+            <button type="submit" onClick={handleLogin}>Login</button>
+          </form>
+        </section>
+        <section>
+          <h2>Signup</h2>
+          <form>
+            <input type="text" placeholder='First name' name='firstName' />
+            <input type="text" placeholder='Last name' name='lastName' />
+            <input type="email" placeholder='Email' name='email' />
+            <input type="password" placeholder='Password' name='password' />
+            <input type="password" placeholder='Confirm Password' name='confirmPassword' />
+            <button type="submit" onClick={handleSignup}>Signup</button>
+          </form>
+        </section>
+      </main>
+    </div>
   )
 }
 
