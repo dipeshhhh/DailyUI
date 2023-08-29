9f05a38437ebc4f58abbf2c41f69a026842c369b
Dipesh <kvdipesh@gmail.com>
2023-08-29 22:46:44 +0530

    Save: work on bill

--- src/Challanges/002_CreditCardCheckout/CreditCardCheckout.js
+++ src/Challanges/002_CreditCardCheckout/CreditCardCheckout.js
@@ -3,24 +3,30 @@ import './CreditCardCheckout.css';
 
 function CreditCardCheckout() {
 
+  const handleCreditCardInput = (event) => {
+    const input = event.target;
+    const formattedInput = input.value.replace(/\D/g, '').slice(0, 4);
+    input.value = formattedInput;
+  }
+
   const bill = [
     {
       _id: '123ABC',
       item_name: 'Laptop',
       quantity: 1,
-      costForOne: 50000,
+      rate: 50000,
     },
     {
       _id: '456DEF',
       item_name: 'Display port to HDMI adapter',
       quantity: 1,
-      costForOne: 264,
+      rate: 264,
     },
     {
       _id: '789GHI',
       item_name: 'USB Stick',
       quantity: 2,
-      costForOne: 657,
+      rate: 657,
     },
   ]
 
@@ -31,27 +37,29 @@ function CreditCardCheckout() {
       <section className='credit-card-bill-box'>
         <h2>Bill</h2>
         {bill ?
-          (<table>
+          (<table className='credit-card-bill-table'>
             <thead>
               <tr>
                 <th>Item</th>
+                <th>Rate</th>
                 <th>Qty.</th>
-                <th>Price x Qty.</th>
+                <th>Amount</th>
               </tr>
             </thead>
             <tbody>
               {bill.map(item => (
                 <tr key={item._id}>
                   <td>{item.item_name}</td>
+                  <td>{item.rate}</td>
                   <td>x{item.quantity}</td>
-                  <td>{item.costForOne * item.quantity}</td>
+                  <td>{item.rate * item.quantity}</td>
                 </tr>
               ))}
             </tbody>
             <tfoot>
               <tr>
-                <td colSpan={2}>Total</td>
-                <td>{bill.reduce(((total, item) => total + item.costForOne * item.quantity), 0)}</td>
+                <th colSpan={3}>Total</th>
+                <th>{bill.reduce(((total, item) => total + item.rate * item.quantity), 0)}</th>
               </tr>
             </tfoot>
           </table>)
@@ -63,40 +71,56 @@ function CreditCardCheckout() {
       {/* A credit card shaped box for entering credit card info
             - position fixed to the top right section (i.e. stays there)
       */}
-      <section className='credit-card-box'>
+      {/* <section className='credit-card-box'>
         <form className='credit-card-input'>
           <section>
             <div id='credit-card-number-input-box'>
               <input
                 id='credit-card-number-part-1'
                 name='credit-card-number'
-                type='number'
+                className='credit-card-number'
+                type='text'
                 size='4'
                 maxLength='4'
+                pattern='[0-9]{4}'
+                inputMode='numeric'
+                onInput={handleCreditCardInput}
                 required
               />
               <input
                 id='credit-card-number-part-2'
                 name='credit-card-number'
-                type='number'
+                className='credit-card-number'
+                type='text'
                 size='4'
                 maxLength='4'
+                pattern='[0-9]{4}'
+                inputMode='numeric'
+                onInput={handleCreditCardInput}
                 required
               />
               <input
                 id='credit-card-number-part-3'
                 name='credit-card-number'
-                type='number'
+                className='credit-card-number'
+                type='text'
                 size='4'
                 maxLength='4'
+                pattern='[0-9]{4}'
+                inputMode='numeric'
+                onInput={handleCreditCardInput}
                 required
               />
               <input
                 id='credit-card-number-part-4'
                 name='credit-card-number'
-                type='number'
+                className='credit-card-number'
+                type='text'
                 size='4'
                 maxLength='4'
+                pattern='[0-9]{4}'
+                inputMode='numeric'
+                onInput={handleCreditCardInput}
                 required
               />
             </div>
@@ -129,7 +153,7 @@ function CreditCardCheckout() {
           </section>
           <button type='submit' > Pay </button>
         </form>
-      </section>
+      </section> */}
     </div>
   )
 }
